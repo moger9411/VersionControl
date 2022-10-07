517ed24bb6c8defcf0a6ddc0e5ce5ab2bc4ac6db
moger9411 <mate.moger@stud.uni-corvinus.hu>
2022-10-07 10:38:45 +0200

    Itt a vege

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -23,8 +23,29 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
-            //CreateExcel();
+            LoadData();
+            CreateExcel();
             CreateTable();
+            
+            
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
 
         }
 
@@ -40,6 +61,10 @@ namespace WindowsFormsApp2
                 "Alapterület (m2)",
                 "Ár (mFt)",
                 "Négyzetméter ár (Ft/m2)"};
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
             object[,] values = new object[Flats.Count, headers.Length];
             int counter = 0;
             foreach (Flat f in Flats)
@@ -48,13 +73,36 @@ namespace WindowsFormsApp2
                 values[counter, 1] = f.Vendor;
                 values[counter, 2] = f.Side;
                 values[counter, 3] = f.District;
-                values[counter, 4] = f.Elevator;
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+         
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
                 values[counter, 8] = "";
                 counter++;
             }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+            xlSheet.get_Range(
+            GetCell(2, 9),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = "=1000000*H2/G2";
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
 
         private void CreateExcel()
