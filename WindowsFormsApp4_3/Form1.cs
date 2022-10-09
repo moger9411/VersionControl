70ea9aa7718e11ae278f86d7d184bab3bf152da9
Máté Móger <mate.moger@stud.uni-corvinus.hu>
2022-10-09 20:01:53 +0200

    Adatok exportálása

--- WindowsFormsApp4_3/Form1.cs
+++ WindowsFormsApp4_3/Form1.cs
@@ -19,12 +19,76 @@ namespace WindowsFormsApp4_3
         Excel.Application xlApp;
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
-        
+        string[] headers = new string[] {
+                     "Kód",
+                     "Eladó",
+                     "Oldal",
+                     "Kerület",
+                     "Lift",
+                     "Szobák száma",
+                     "Alapterület (m2)",
+                     "Ár (mFt)",
+                     "Négyzetméter ár (Ft/m2)"};
         public Form1()
         {
             InitializeComponent();
             LoadData();
             CreateExcel();
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+            object[,] values = new object[Flats.Count, headers.Length];
+            int sorszam = 0;
+            foreach (var flat in Flats)
+            {
+                values[sorszam, 0] = flat.Code;
+                values[sorszam, 1] = flat.Vendor;
+                values[sorszam, 2] = flat.Side;
+                values[sorszam, 3] = flat.District;
+                if (flat.Elevator)
+                {
+                    values[sorszam, 4] = "Van";
+                }
+                else
+                {
+                    values[sorszam, 4] = "Nincs";
+                };
+                values[sorszam, 5] = flat.NumberOfRooms;
+                values[sorszam, 6] = flat.FloorArea;
+                values[sorszam, 7] = flat.Price;
+                values[sorszam, 8] = "";
+                sorszam++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+            xlSheet.get_Range(
+            GetCell(2, 9),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = "=1000000*H2/G2";
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
         private void CreateExcel()
@@ -41,7 +105,7 @@ namespace WindowsFormsApp4_3
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-                //CreateTable(); // Ennek megírása a következő feladatrészben következik
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
