6a6b31f5dc41e1a523a0ac34615522cb0bcd5466
Máté Móger <mate.moger@stud.uni-corvinus.hu>
2022-10-11 11:57:39 +0200

    Form design

--- WindowsFormsApp3het_uj/Form1.cs
+++ WindowsFormsApp3het_uj/Form1.cs
@@ -15,6 +15,9 @@ namespace WindowsFormsApp3het_uj
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
         }
     }
 }
