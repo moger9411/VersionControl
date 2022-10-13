302c6a4616fa8b7df0d2c89bf3000d342c29d01d
Máté Móger <mate.moger@stud.uni-corvinus.hu>
2022-10-13 10:42:58 +0200

    Adatmodell

---
+++ mintazh/Mappa/OlympicResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mintazh.Mappa
+{
+    internal class OlympicResult
+    {
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public int[] Medals { get; set; }
+        public int Position { get; set; }
+
+    }
+}
