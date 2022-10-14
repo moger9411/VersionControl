8f83fb5c85d688a34b2e10b2074e855c7b4451c1
moger9411 <mate.moger@stud.uni-corvinus.hu>
2022-10-14 10:06:11 +0200

    Összegzés osztály

---
+++ zh_g3w0i5/Mappa/CategorySummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh_g3w0i5.Mappa
+{
+    class CategorySummary
+    {
+        public string Name { get; set; }
+        public int NbrOfSales { get; set; }
+        public decimal TotalSales { get; set; }
+
+    }
+}
