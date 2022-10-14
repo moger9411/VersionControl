276d24e82845cae5bf404d26252183dd376afc36
moger9411 <mate.moger@stud.uni-corvinus.hu>
2022-10-14 10:03:56 +0200

    Adatbázis csatlakozás

---
+++ zh_g3w0i5/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zh_g3w0i5
+{
+    public partial class Form1 : Form
+    {
+        List<DVD_RentalEntities> context = new List<DVD_RentalEntities>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
