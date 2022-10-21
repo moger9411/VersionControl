fe0d3a49e0bcb8649660a49e6252b46d259b98d1
moger9411 <mate.moger@stud.uni-corvinus.hu>
2022-10-21 10:12:41 +0200

    3. Webszolgáltatás hívása

--- G3W0I5_5gyak/Form1.cs
+++ G3W0I5_5gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using G3W0I5_5gyak.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace G3W0I5_5gyak
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            WebCall();
+        }
+
+        private string WebCall()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+            return result;
         }
     }
 }
