0271300ce053d72073aca490231a429a8be892fd
moger9411 <mate.moger@stud.uni-corvinus.hu>
2022-10-21 10:18:40 +0200

    4. Árfolyam lista létrehozása

--- G3W0I5_5gyak/Form1.cs
+++ G3W0I5_5gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using G3W0I5_5gyak.ServiceReference1;
+﻿using G3W0I5_5gyak.Entities;
+using G3W0I5_5gyak.ServiceReference1;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,11 +16,19 @@ namespace G3W0I5_5gyak
 {
     public partial class Form1 : Form
     {
-
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
         public Form1()
         {
             InitializeComponent();
             WebCall();
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            dataGridView1.DataSource = Rates;
         }
 
         private string WebCall()
